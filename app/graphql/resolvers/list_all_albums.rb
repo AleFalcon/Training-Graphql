3fe9d5a7111f1b4847d1e7aeec584a04f5335b1e
AleFalcon <alex.falcon@wolox.com.ar>
2020-01-14 14:59:56 -0300

    Clean PR and fixed by rubocop

--- app/graphql/resolvers/list_all_albums.rb
+++ app/graphql/resolvers/list_all_albums.rb
@@ -1,13 +1,15 @@
 require 'search_object/plugin/graphql'
 
-class Resolvers::ListAllAlbums
-  include SearchObject.module(:graphql)
+module Resolvers
+  class ListAllAlbums
+    include SearchObject.module(:graphql)
 
-  scope { HTTParty.get('https://jsonplaceholder.typicode.com/albums').parsed_response }
+    scope { HTTParty.get('https://jsonplaceholder.typicode.com/albums').parsed_response }
 
-  type types[Types::AlbumType]
+    type types[Types::AlbumType]
 
-  def apply_filter
-    scope
+    def apply_filter
+      scope
+    end
   end
 end
