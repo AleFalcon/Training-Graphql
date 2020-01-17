31372cc450110a08f61429b5a086906c1a39e68b
AleFalcon <alex.falcon@wolox.com.ar>
2020-01-17 17:07:00 -0300

    More corrections

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -2,6 +2,6 @@ module Types
   class BaseObject < GraphQL::Schema::Object
     field_class Types::BaseField
 
-    URL_FOR_ALBUM = 'https://jsonplaceholder.typicode.com/todos/'.freeze
+    URL_FOR_ALBUM = 'https://jsonplaceholder.typicode.com/albums/'.freeze
   end
 end
