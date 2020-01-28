1225a2f4a815ca35d0dff4f6b9cccee77d3715ad
AleFalcon <alex.falcon@wolox.com.ar>
2020-01-28 13:24:44 -0300

    clean file QueryTypes

---
+++ app/services/albums_service.rb
@@ -0,0 +1,18 @@
+class AlbumsService
+
+  def get_album(id, endpoint)
+    album = parse_album(HTTParty.get("#{endpoint}albums/#{id}"))
+    album[:photos] = parse_photos(HTTParty.get("#{endpoint}photos?albumId=#{id}"))
+    album
+  end
+
+  private
+
+  def parse_album(response)
+    response.parsed_response.transform_keys(&:underscore)
+  end
+
+  def parse_photos(response)
+    response.parsed_response.map { |x| x.transform_keys(&:underscore) }
+  end
+end
