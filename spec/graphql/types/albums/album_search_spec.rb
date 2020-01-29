c51dfaece5a70c0086dbf073fc7eb6e7c4849bd2
AleFalcon <alex.falcon@wolox.com.ar>
2020-01-29 13:29:10 -0300

    Many fixes by comments in github

--- spec/graphql/types/albums/album_search_spec.rb
+++ spec/graphql/types/albums/album_search_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'support/graphql/fixtures/graphql_album_helper'
 
 module Types
   module Albums
--- spec/support/mock_herlper.rb
+++ spec/support/mock_herlper.rb
@@ -11,7 +11,7 @@ module MockHelper
   def album_search_request_not_found
     stub_request(:get, 'https://jsonplaceholder.typicode.com/albums/10000000')
       .to_return(
-        status: 200,
+        status: 404,
         body: File.read('./spec/support/fixtures/mocked_album_search_not_found.json'),
         headers: { 'Content-Type': 'application/json' }
       )
@@ -29,7 +29,7 @@ module MockHelper
   def photos_for_album_not_found
     stub_request(:get, 'https://jsonplaceholder.typicode.com/photos?albumId=10000000')
       .to_return(
-        status: 200,
+        status: 404,
         body: File.read('./spec/support/fixtures/mocked_photos_album_not_found.json'),
         headers: { 'Content-Type': 'application/json' }
       )
