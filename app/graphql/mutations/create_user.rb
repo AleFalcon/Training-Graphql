c9ec424471843f6f7f1130b4d12afe10cfd1afd0
AleFalcon <alex.falcon@wolox.com.ar>
2020-01-29 15:11:36 -0300

    Fixed by rubocop

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -8,18 +8,22 @@ module Mutations
     type Types::UserType
 
     def resolve(first_name: nil, last_name: nil, email: nil, password: nil)
-      user = {
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        password: password
-      }
+      user = create_hash_user(first_name, last_name, email, password)
       result = UserPolicy.new(nil, user).create?
       unless result[:result]
         raise Pundit::NotAuthorizedError, "not allowed to create? this #{result[:message]}"
       end
 
       User.create!(user)
+    end
+
+    def create_hash_user(first_name, last_name, email, password)
+      {
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password
+      }
     end
   end
 end
