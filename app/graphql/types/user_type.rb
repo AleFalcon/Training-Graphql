fb4063a4a7c0be094be035f695bdb6d1009721c4
AleFalcon <alex.falcon@wolox.com.ar>
2020-01-29 16:07:10 -0300

    Field deprecated first_name and last_name

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,8 +1,10 @@
 module Types
   class UserType < BaseObject
     field :id, ID, null: false
-    field :first_name, String, null: false
-    field :last_name, String, null: false
+    field :first_name, String, null: false,
+                               deprecation_reason: 'Deprecated because the card requests it'
+    field :last_name, String, null: false,
+                              deprecation_reason: 'Deprecated because the card requests it'
     field :email, String, null: false
   end
 end
