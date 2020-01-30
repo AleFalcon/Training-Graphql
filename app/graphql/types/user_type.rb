c2f0828966f3d44526821c375fbf5c3665c9fea0
AleFalcon <alex.falcon@wolox.com.ar>
2020-01-30 12:30:52 -0300

    Creation field name

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,5 +6,11 @@ module Types
     field :last_name, String, null: false,
                               deprecation_reason: 'Deprecated because the card requests it'
     field :email, String, null: false
+    field :name, String, null: false,
+                         description: 'Field that concatenates the first_name and the last_name'
+
+    def name
+      object.first_name + ' ' + object.last_name
+    end
   end
 end
