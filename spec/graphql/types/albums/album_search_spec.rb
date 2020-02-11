77ec326178003c332088f195a1f4b49b419aef4c
AleFalcon <alex.falcon@wolox.com.ar>
2020-02-11 12:00:14 -0300

    Correction by Rubocop

--- spec/graphql/types/albums/album_search_spec.rb
+++ spec/graphql/types/albums/album_search_spec.rb
@@ -10,7 +10,7 @@ module Types
               'title' => 'accusamus beatae ad facilis cum similique qui sunt',
               'url' => 'https://via.placeholder.com/600/92c952' }
           end
-          
+
           let(:expect_album) do
             { 'id' => '1',
               'photos' => [
