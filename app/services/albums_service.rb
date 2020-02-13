fa676aea9b91da49aeff20f866e32a434daacc88
AleFalcon <alex.falcon@wolox.com.ar>
2020-02-13 13:38:55 -0300

    Corrections on methods AlbumsServices

--- app/services/albums_service.rb
+++ app/services/albums_service.rb
@@ -7,11 +7,21 @@ class AlbumsService
 
   def get_album(id)
     album = parse_albums(HTTParty.get("#{endpoint}albums/#{id}"))
-    album[:photos] = PhotosService.new.parse_photos(HTTParty.get("#{endpoint}photos?albumId=#{id}"))
+    album[:photos] = PhotosService.new(endpoint).get_photo(id)
     album
   end
 
+  def list_all_albums
+    parse_all_albums(HTTParty.get("#{endpoint}albums/"))
+  end
+
+  private
+
   def parse_albums(response)
+    response.parsed_response.transform_keys(&:underscore)
+  end
+
+  def parse_all_albums(response)
     response.parsed_response.map { |x| x.transform_keys(&:underscore) }
   end
 end
