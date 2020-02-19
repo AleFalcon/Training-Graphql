b3671c8d60b7c593e50f2b85305478fa4e5ffbe9
AleFalcon <alex.falcon@wolox.com.ar>
2020-02-19 17:11:32 -0300

    Use cookies per session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
   around_action :switch_locale
 
   def switch_locale(&action)
